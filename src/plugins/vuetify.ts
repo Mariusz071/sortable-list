0897e0486dd64e4b427de28edbdcb959c2f0aa4c
Mariusz <siewruk.mariusz92@gmail.com>
2023-10-04 20:49:56 +0200

    Initial setup

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,27 @@
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: '##26FF90',
+          secondary: '#6357B1',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+          disabled: '#BBBBBB'
+        }
+      }
+    }
+  }
+})
